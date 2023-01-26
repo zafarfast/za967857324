a010bd5ffc731c47d433b1e5d20e5d332806415a
zafarfast <zafar_fast@hotmail.com>
2023-01-26 15:05:52 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,139 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+let userInput = {
+    projectName : "",
+    projectDescription : "",
+    installationIns : "",
+    usage : "",
+    license : "",
+    contribute : "",
+    test : "",
+    github : "",
+    email : ""
+   }
+
+   
+inquirer.prompt([
+    {
+        name: "projectName",
+        type: "input",
+        message: "Enter project name: "
+  
+    },
+    {
+        name: "projectDescription",
+        type: "input",
+        message: "Enter project description: "
+  
+    },
+    {
+        name: "installationInstructions",
+        type: "input",
+        message: "Enter installationdata instructions: "
+  
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "Enter usage instructions: "
+  
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Choose license: ",
+        choices: ["MIT license", "GNU license", "Apache license", "Mozilla Public license"],
+        default: 'MIT license'
+  
+    },
+    {
+        name: "contribute",
+        type: "input",
+        message: "Describe how others can contribute to this project: "
+  
+    },
+    {
+        name: "test",
+        type: "input",
+        message: "Enter test instructions: "
+  
+    },
+    {
+        name: "github",
+        type: "input",
+        message: "Enter project's Github URL: "
+  
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "Enter your email address: "
+  
+    }
+
+
+
+]).then((response)=>{
+
+    userInput.projectName = response.projectName;
+    userInput.projectDescription = response.projectDescription;
+    userInput.installationIns = response.installationInstructions;
+    userInput.usage = response.usage;
+    userInput.license = response.license;
+    userInput.contribute = response.contribute;
+    userInput.test = response.test
+    userInput.github = response.github
+    userInput.email = response.email
+
+    let readmeMarkup = `# Project Title
+   ${userInput.projectName}
+   
+   
+   ## Description
+   ${userInput.projectDescription}
+   
+   
+   ## Table of Contents
+   
+   [Installation](#installation)
+   
+   [Usage](#usage)
+   
+   [License](#license)
+   
+   [Contributing](#contributing)
+   
+   [Tests](#tests)
+   
+   [Questions](#questions)
+   
+   
+   
+   
+   ## Installation
+   ${userInput.installationIns}
+   
+   ## Usage
+   ${userInput.usage}
+   
+   ## License 
+   ${userInput.license}
+   
+   ## Contributing 
+   ${userInput.contribute}
+   
+   ## Tests 
+   ${userInput.test}
+   
+   ## Questions
+   
+   Github: ${userInput.github}
+       
+   If you wish to reach out to me, my email adddress is <a href='mailto:${userInput.email}'> ${userInput.email} </a>
+      
+    `;
+
+
+    fs.writeFile('readme2.md',readmeMarkup,(err)=>{console.log(err)})
+})
